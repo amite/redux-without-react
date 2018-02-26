bd095a54d54a3538c0b8c5fd47568de54f0fd265
amite <amit.erandole@gmail.com>
2018-02-26 07:58:06 +0530

    initial commit with basic store

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const redux = require("redux");
+
+const initialState = {
+  profile: {
+    gender: "female",
+    name: {
+      title: "mrs",
+      first: "dânia",
+      last: "da mota"
+    },
+    phone: "015394 16261",
+    cell: "0757-275-048",
+    location: {
+      street: "2591 rua pará ",
+      city: "teixeira de freitas",
+      state: "mato grosso",
+      postcode: 31682
+    },
+    currentPlan: "free",
+    upgraded: false,
+    loggedIn: false,
+    verified: false
+  },
+  plans: [
+    {
+      id: "33453ttt",
+      name: "gold",
+      cost: 3500
+    },
+    {
+      id: "334545tt",
+      name: "silver",
+      cost: 2500
+    }
+  ]
+};
+
+const userReducer = function(state = initialState) {
+  return state;
+};
+
+const store = redux.createStore(userReducer);
+
+console.log(store.getState());
