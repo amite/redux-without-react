8084afcfddd3040f8543c5b4ecdc70f3e975788f
amite <amit.erandole@gmail.com>
2018-02-27 10:23:38 +0530

    subscribes with render

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ const { updateName } = require("./actions");
 
 const store = redux.createStore(rootReducer);
 
-store.dispatch(
-  updateName({
-    title: "Ms",
-    first: "dana",
-    last: "eggling"
-  })
-);
-
 const jsonRes = JSON.stringify(store.getState());
-
+console.log("BEFORE");
 console.log(jsonRes);
+
+setTimeout(() => {
+  store.dispatch(
+    updateName({
+      title: "Ms",
+      first: "dana",
+      last: "eggling"
+    })
+  );
+}, 1500);
+
+const render = () => {
+  const jsonRes = JSON.stringify(store.getState());
+  console.log("AFTER");
+  console.log(jsonRes);
+};
+
+store.subscribe(render);
